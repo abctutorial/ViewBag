e4f615ba0157ad820eef48baf7ad9fe6df14b9de
abctutorial <abctutorial.com@gmail.com>
2016-07-14 10:40:39 +0600

    first commit

---
+++ ListWithoutModelPass/ListWithoutModelPass/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using ListWithoutModelPass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListWithoutModelPass.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            ContactDBEntities db = new ContactDBEntities();
+            ViewBag.ItemData = db.tblAddresses.ToList();
+            return View();
+        }
+    }
+}
---
+++ ListWithoutModelPass/ListWithoutModelPass/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ListWithoutModelPass.Startup))]
+namespace ListWithoutModelPass
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
